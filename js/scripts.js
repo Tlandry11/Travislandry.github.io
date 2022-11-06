d238268e00957f2e0f6b51f623f21635addeaee0
tlandry11 <Madeworldwide00@gmail.com>
2022-11-06 01:21:34 -0500

    2.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 console.log("Hello, welcome to my portfolio site. Hope you enjoy your visit!");
+$(".button").on("click", function() {
+  alert("This button has been clicked!");
+});
+function menuToggle() {
+  var x = document.getElementById('mynavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+  
