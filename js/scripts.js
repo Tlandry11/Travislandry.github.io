3ee7d548f67cf65e745fa26c1faa8f8553000445
tlandry11 <Madeworldwide00@gmail.com>
2022-11-06 11:13:49 -0600

    2.3
    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,4 @@
 console.log("Hello, welcome to my portfolio site. Hope you enjoy your visit!");
-$(".button").on("click", function() {
-  alert("This button has been clicked!");
-});
 function menuToggle() {
   var x = document.getElementById('mynavtoggle');
   if (x.className === 'navtoggle') {
